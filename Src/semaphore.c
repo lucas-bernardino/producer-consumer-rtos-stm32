39ea37929f0f2b35a383f6bfecfff24d48e14b91
lucas-bernardino <lucasgber@gmail.com>
2024-09-15 13:28:43 -0300

    Add __disable_irq() and __enable_irq() between critical sections

--- Src/semaphore.c
+++ Src/semaphore.c
@@ -6,17 +6,28 @@
  */
 
 #include <stdint.h>
+#include <cmsis_gcc.h>
 #include "semaphore.h"
-
 
 void sem_init(semaphore* s, uint8_t init_value) {
 	s->sem_count = init_value;
 }
 
 void sem_wait(semaphore* s) {
+	// precisa ter um "mutex lock"
+	__disable_irq();
+	while (s->sem_count <= 0) {
+		// Precisa achar um jeito de ficar esperando até ele for maior que zero ?
+	}
 	s->sem_count--;
+	__enable_irq();
+	// precisa ter um "mutex unlock"
 }
 
 void sem_post(semaphore* s) {
+	// precisa ter um "mutex lock"
+	__disable_irq();
 	s->sem_count++;
+	// precisa ter um "mutex unlock"
+	__enable_irq();
 }
